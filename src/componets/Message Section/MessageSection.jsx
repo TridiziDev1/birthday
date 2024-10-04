f4ae4c29008423c051cd9d0a9b42bc27847f32f0
krishnamohan645 <krishnaguduri08@gmail.com>
2024-10-04 16:43:28 +0530

    css changes

--- src/componets/Message Section/MessageSection.jsx
+++ src/componets/Message Section/MessageSection.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import "./MessageSection.css"
-import elipse from "../../assets/Ellipse 1436.png"
-import img1 from "../../assets/WhatsApp Image 2024-09-30 at 11.06.00 (1) 1.png"
-import img2 from "../../assets/WhatsApp Image 2024-09-30 at 11.06.02 (2) 1.png"
+import React from "react";
+import "./MessageSection.css";
+import elipse from "../../assets/Ellipse 1436.png";
+import img1 from "../../assets/WhatsApp Image 2024-09-30 at 11.06.00 (1) 1.png";
+import img2 from "../../assets/WhatsApp Image 2024-09-30 at 11.06.02 (2) 1.png";
 const MessageSection = () => {
   return (
-    <section className="message-section">
-      <p className="message">
-        Happy Birthday to the most amazing boss 🎉! You're like the ultimate combination of a wise mentor and the fun friend we all secretly wish we had. 
-        May your birthday be filled with laughter, great food, and all the happiness you deserve. Here's to many more years of your leadership and that one-of-a-kind spirit! 🎂❤️
-      </p>
-
+    <section id="about" className="message-section">
+      <div className="message_div">
+        <p className="message">
+          Happy Birthday to the most amazing boss! 🎉 You're like the ultimate
+          combination of a wise mentor and that fun friend we all secretly wish
+          we had. We admire how you approach every challenge with the energy of
+          a kid in a candy store – full of excitement and endless curiosity!
+          Your knowledge never ceases to amaze us, and your kindness makes you
+          the heart of our team. Thank you for always looking out for us and
+          making work feel like a second home. May your birthday be filled with
+          laughter, great food (we know you love it!), and all the happiness you
+          deserve. Cheers to many more years of your leadership and that
+          one-of-a-kind spirit! 🎂🎈{" "}
+        </p>
+      </div>
       <div className="image-grid">
         <img src={elipse} alt="img1" />
         {/* <div className='img-container'> */}
-        <img className='message-img1' src={img1} alt="img2" />
+        <img className="message-img1" src={img1} alt="img2" />
         {/* </div> */}
         {/* <div className='img-container1'> */}
         <img src={img2} alt="img3" />
         {/* </div> */}
-        
-       <h1 className='h1-title'>Naughty</h1>
-       <h1 className='h1-title-1'>Angry Bird</h1>
+
+        <h1 className="h1-title">Naughty</h1>
+        <h1 className="h1-title-1">Angry Bird</h1>
         {/* Replace with images as per your requirement */}
         {/* <div className="grid-item"><img src={} alt="Image 1" /></div>
         <div className="grid-item"><img src="/path/to/image2.jpg" alt="Image 2" /></div>
@@ -32,6 +41,6 @@ const MessageSection = () => {
       <p className="from-team">- From your well-wishing team</p> */}
     </section>
   );
-}
+};
 
 export default MessageSection;
--- src/componets/NavBar/Navbar.jsx
+++ src/componets/NavBar/Navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import "./NavBar.css"
 
 function Navbar() {
+  // const handleClick =()=>{
+    
+  // }
   return (
     <div className='Main_Nav'>
         <p className='Name'>Sandeep</p>
         <div className='Wishes_Banner'>
             {/* <p>Wishes</p> */}
-            <p>About B’day Star</p>
-            <p>Memory Line</p>
+            <a href="#about" className="nav-link">About B’day Star</a>
+            <a href="#memory" className="nav-link">Memory Line</a>
         </div>
     </div>
   )
